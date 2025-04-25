b8eeda6c1c4ba284f1f7d11b07d4c48e23a50e49
ddz95 <dawid.dziedzic95@gmail.com>
2025-04-25 19:33:11 +0200

    Setting up AWS provider.

---
+++ devops-project/infrastructure/terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "default_region" {
+    description = "Default AWS region"
+    type = string
+    default = "eu-central-1"
+}
+
+variable "aws_region" {
+    description = "AWS region"
+    type = string
+    default = ""
+}
+
+variable "aws_access_key" {
+    description = "AWS access key"
+    type = string
+    default = ""
+}
+
+variable "aws_secret_key" {
+    description = "AWS secret key"
+    type = string
+    default = ""
+}
+
+variable "aws_profile" {
+    description = "AWS profile name"
+    type = string
+    default = ""
+}
+
+variable "aws_account_id"{
+    description = "AWS account ID"
+    type = string
+    default = ""
+}
