354cc0a9486a4b47e84e7192fec42dc595c6eb84
ddz95 <dawid.dziedzic95@gmail.com>
2025-05-20 21:13:02 +0200

    feat(dev): added ec2 templates on dev environment

---
+++ devops-project/infrastructure/terraform/environments/dev/ec2.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "ec2" {
+    ami = var.ec2_ami_id
+    instance_type = var.ec2_instance_type
+    tags = {
+        Name = "Hello World!"
+    }
+}
---
+++ devops-project/infrastructure/terraform/environments/dev/variables.tf
@@ -0,0 +1,11 @@
+variable "ec2_instance_type" {
+    description = "EC2 instance type"
+    type = string
+    default = "t2.micro"
+}
+
+variable "ec2_ami_id" {
+    description = "AMI ID for the EC2 instance"
+    type = string
+    default = "ami-0c55b159cbfafe1f0"
+}
