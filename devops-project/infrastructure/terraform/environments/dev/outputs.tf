d6e48c846af78c9ccd4e08d6ec889a2684f3beba
ddz95 <dawid.dziedzic95@gmail.com>
2025-05-20 21:36:05 +0200

    fix(dev): terraform format

--- devops-project/infrastructure/terraform/environments/dev/outputs.tf
+++ devops-project/infrastructure/terraform/environments/dev/outputs.tf
@@ -1,4 +1,4 @@
 output "ec2_instance_ip_address" {
-    value = aws_instance.ec2.private_ip
-    description = "Private IP address of the EC2 instance"
+  value       = aws_instance.ec2.private_ip
+  description = "Private IP address of the EC2 instance"
 }
