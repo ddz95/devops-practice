3f3361af7917e121ec69e70a9f25ec010d194cc7
ddz95 <dawid.dziedzic95@gmail.com>
2025-05-25 22:30:20 +0200

    feat: basic ec2 module

---
+++ devops-project/infrastructure/terraform/modules/ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-00a44bd8918c7ee43" # ? Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+}
