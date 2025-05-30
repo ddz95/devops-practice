67d912e8cc3c28c048e647958a12cba94427ff16
ddz95 <dawid.dziedzic95@gmail.com>
2025-05-30 21:53:06 +0200

    fix: deleted unnecessary variables, changed variable descriptions

--- devops-project/infrastructure/terraform/variables.tf
+++ devops-project/infrastructure/terraform/variables.tf
@@ -1,35 +1,23 @@
 variable "default_region" {
-  description = "Default AWS region"
+  description = "Default AWS region to use if not specified."
   type        = string
   default     = "eu-central-1"
 }
 
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region to deploy resources in."
   type        = string
   default     = ""
 }
 
 variable "aws_access_key" {
-  description = "AWS access key"
+  description = "AWS access key. This is used for authentication with AWS services."
   type        = string
   default     = ""
 }
 
 variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  default     = ""
-}
-
-variable "aws_profile" {
-  description = "AWS profile name"
-  type        = string
-  default     = ""
-}
-
-variable "aws_account_id" {
-  description = "AWS account ID"
+  description = "AWS secret key. This is used for authentication with AWS services."
   type        = string
   default     = ""
 }
