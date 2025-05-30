52b062e44e422dfaaf1b2a291dfa253ac6a7afc9
ddz95 <dawid.dziedzic95@gmail.com>
2025-05-30 21:46:46 +0200

    feat: added instance_state and tags variables

--- devops-project/infrastructure/terraform/modules/ec2/variables.tf
+++ devops-project/infrastructure/terraform/modules/ec2/variables.tf
@@ -1,11 +1,23 @@
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "EC2 instance type."
   type        = string
   default     = "t2.micro"
 }
 
 variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
+  description = "AMI ID for the EC2 instance."
   type        = string
-  default     = "ami-00a44bd8918c7ee43" # ? Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+  default     = "ami-00a44bd8918c7ee43" # * Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+}
+
+variable "instance_state" {
+  description = "State of the EC2 instance."
+  type = string
+  default = "running"
+}
+
+variable "tags" {
+  description = "Optional map of tags for the EC2 instance."
+  type        = map(string)
+  default     = {}
 }
