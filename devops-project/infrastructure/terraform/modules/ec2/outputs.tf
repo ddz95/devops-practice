75e35ca361c10088b6bd6374505b0a04ae738a4a
ddz95 <dawid.dziedzic95@gmail.com>
2025-05-30 21:47:52 +0200

    feat: added instance_state and tags outputs

--- devops-project/infrastructure/terraform/modules/ec2/outputs.tf
+++ devops-project/infrastructure/terraform/modules/ec2/outputs.tf
@@ -1,4 +1,14 @@
-output "private_ip_address" {
+output "private_ip" {
   value       = aws_instance.ec2.private_ip
   description = "Private IP address of the EC2 instance"
+}
+
+output "instance_state" {
+  value = aws_instance.ec2.instance_state
+  description = "State of the EC2 instance"
+}
+
+output "tags" {
+  value = aws_instance.ec2.tags
+  description = "Optional tags applied to the EC2 instance"
 }
