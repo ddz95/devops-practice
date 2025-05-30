5730067f41213ae511d6cd0ade7ed713845b2623
ddz95 <dawid.dziedzic95@gmail.com>
2025-05-30 21:41:23 +0200

    style: added tags, comment to ami_id

--- devops-project/infrastructure/terraform/environments/dev/ec2.tf
+++ devops-project/infrastructure/terraform/environments/dev/ec2.tf
@@ -1,5 +1,10 @@
 module "ec2" {
   source            = "../../modules/ec2"
   instance_type = "t2.micro"
-  ami_id       = "ami-00a44bd8918c7ee43"
+  ami_id       = "ami-00a44bd8918c7ee43" # * Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+
+  tags = {
+    Name = "Dev test instance"
+    Environment = "dev"
+  }
 }
