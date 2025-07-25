0098f1223df50821c8757cd31d927a653573aa62
ddz95 <dawid.dziedzic95@gmail.com>
2025-07-25 15:32:30 +0200

    feat: backend S3

---
+++ devops-project/infrastructure/terraform/bootstrap/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "devops-project-bootstrap-s3-bucket"
+    key            = "env/dev/bootstrap/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = ""
+    encrypt        = true
+  }
+}
