c28c391d6f9dfb95613e8f818b87bbe511a1dbe7
ddz95 <dawid.dziedzic95@gmail.com>
2025-08-02 17:49:07 +0200

    fix: format

--- devops-project/infrastructure/terraform/bootstrap/dynamodb.tf
+++ devops-project/infrastructure/terraform/bootstrap/dynamodb.tf
@@ -1,10 +1,10 @@
 module "dynamodb" {
-  source = "../modules/dynamodb"
-  name = "terraform-lock-table"
-  billing_mode = "PROVISIONED"
-  read_capacity = 25
+  source         = "../modules/dynamodb"
+  name           = "terraform-lock-table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 25
   write_capacity = 25
-  hash_key = "LockID"
+  hash_key       = "LockID"
 
   tags = {
     Description = "DynamoDB table for Terraform state locking"
