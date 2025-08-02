0c2c1a834fe20b13a2308c4199563cfe9435343e
ddz95 <dawid.dziedzic95@gmail.com>
2025-08-02 17:51:16 +0200

    fix: format

--- devops-project/infrastructure/terraform/bootstrap/s3.tf
+++ devops-project/infrastructure/terraform/bootstrap/s3.tf
@@ -1,7 +1,7 @@
 module "s3" {
-  source  = "../modules/s3"
-  bucket = "devops-project-bootstrap-s3-bucket"
-  force_destroy = true
+  source            = "../modules/s3"
+  bucket_name       = "devops-project-bootstrap-s3-bucket"
+  force_destroy     = true
   versioning_status = "Enabled"
 
   tags = {
